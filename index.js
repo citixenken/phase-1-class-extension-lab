778ae691323e53d68117482054356aa93e25ab6b
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-29 22:25:09 +0300

    commit tree 3fd6224db112527709815d37d879bf0e0ee5ac33

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Polygon {
+  constructor(array) {
+    this.array = array;
+  }
+
+  get countSides() {
+    return this.array.length;
+  }
+
+  get perimeter() {
+    return this.array.reduce((accum, value) => accum + value, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isvalid() {
+    if (!Array.isArray(this.array)) return;
+    if (this.countSides() !== 3) return;
+    let a = this.array[0];
+    let b = this.array[1];
+    let c = this.array[2];
+    return a + b > c && a + c > b && b + c > a;
+  }
+}
+
+class Square extends Polygon {
+  get isvalid() {
+    if (!Array.isArray(this.array)) return;
+    if (this.countSides() !== 4) return;
+    let a = this.array[0];
+    let b = this.array[1];
+    let c = this.array[2];
+    let d = this.array[3];
+    return a === b && a === c && a === d;
+  }
+
+  get area() {
+    return this.array[0] * this.array[0];
+  }
+}
